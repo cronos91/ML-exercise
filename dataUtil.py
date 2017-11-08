938446ea3aa7d1540b37506940df49fd01b86394
장석인 <seokinj@jangseog-in-ui-MacBook-Pro.local>
2017-11-08 22:50:53 +0900

    add test data

--- dataUtil.py
+++ dataUtil.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
 
-path = '/Users/seokinj/tensorflow/assignments2016/assignment1/cs231n/datasets/cifar-10-batches-py/data_batch_'
+path = '/Users/seokinj/tensorflow/assignments2016/assignment1/cs231n/datasets/cifar-10-batches-py/'
 def load_CIFAR_batch(file):
 	with open(file, 'rb') as fo:
 		dict = pickle.load(fo, encoding='bytes')
@@ -13,11 +13,12 @@ def load_CIFAR_data():
 	xs = []
 	ys = []
 	for x in range(1,6):
-		X,Y = load_CIFAR_batch(path+str(x))
+		X,Y = load_CIFAR_batch(path+'data_batch_'+str(x))
 		xs.append(X)
 		ys.append(Y)
-	print(len(xs))
-	print(len(xs[0]))
-	return np.concatenate(xs).shape
+	x_trains = np.concatenate(xs)
+	y_trains = np.concatenate(ys)
+	x_tests, y_tests = load_CIFAR_batch(path+'test_batch')
+	return x_trains, y_trains, x_tests, y_tests	
 
 print(load_CIFAR_data())
