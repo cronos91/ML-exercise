fc62912e7d67f85badb1c6af6e29ccc6ec19c687
장석인 <seokinj@jangseog-in-ui-MacBook-Pro.local>
2017-12-13 23:54:15 +0900

    update

--- loss.py
+++ loss.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3604baeb08c557ebefe789ba0ce8d4599cb961457079b708981e2f1c3200d055
-size 287
+oid sha256:2b38b2fe9407067f46c73698ea4aa17d5643b6f537de1c495b3dc38ae84aa22c
+size 861
